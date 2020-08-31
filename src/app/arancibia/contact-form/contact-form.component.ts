f56fbc99e5f3c236817557472ea5d716eb442f63
Fabian Orccon <cfoch.fabian@gmail.com>
2020-08-31 00:00:59 -0500

    arancibia: contact-form: Validate forms

--- src/app/arancibia/contact-form/contact-form.component.ts
+++ src/app/arancibia/contact-form/contact-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators, FormGroup } from '@angular/forms';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { invalid } from '@angular/compiler/src/render3/view/util';
 
 interface MailChimpResponse {
   result: string;
@@ -17,16 +18,19 @@ export class ContactFormComponent implements OnInit {
     'post-json?u=79d161c2c6d19193041762737&id=c8e6cca2e8';
   private stepIndex = 0;
   submitted = false;
+  displayValidation = false;
   error = null;
 
   formData = new FormGroup({
     firstNameControl: new FormControl('', [
       Validators.required,
+      Validators.minLength(3),
     ]),
     lastNameControl: new FormControl('', [
       Validators.required,
     ]),
     dniControl: new FormControl('', [
+      Validators.required,
       Validators.minLength(8),
       Validators.maxLength(8),
     ]),
@@ -38,7 +42,6 @@ export class ContactFormComponent implements OnInit {
       Validators.email
     ]),
     messageControl: new FormControl('', [
-      Validators.required,
     ]),
     wantsInstallmentsControl: new FormControl(false, [
       Validators.required,
@@ -59,25 +62,44 @@ export class ContactFormComponent implements OnInit {
   }
 
   rollStep(delta: number) {
+    if (!this.validateForm()) {
+      return;
+    }
     this.stepIndex += delta;
   }
 
+  validateForm() {
+    if (this.formData.invalid) {
+      this.displayValidation = true;
+      return false;
+    }
+    return true;
+  }
+
   onSubmit(data) {
-    const params = new HttpParams()
+    if (!this.validateForm()) {
+      return false;
+    }
+
+    let params = new HttpParams()
       .set('FIRST_NAME', data.firstNameControl)
       .set('LAST_NAME', data.lastNameControl)
       .set('DNI', data.dniControl)
       .set('EMAIL', data.emailControl)
       .set('PHONE', data.phoneControl)
-      .set('MESSAGE', data.messageControl)
-      .set('b_79d161c2c6d19193041762737_c8e6cca2e8', ''); // hidden input name
+      .set('MESSAGE', data.messageControl);
+    if (data.wantsInstallmentsControl) {
+      params = params.set('DOWN_PAY', data.downPaymentControl);
+      params = params.set('BANK_CRED', data.bankCreditControl);
+    }
+    // hidden input name
+    params = params.set('b_79d161c2c6d19193041762737_c8e6cca2e8', '');
 
     const mailChimpUrl = this.mailChimpEndPoint + '&' + params.toString();
 
-    console.log(mailChimpUrl);
-
     this.http.jsonp<MailChimpResponse>(mailChimpUrl, 'c').subscribe(response => {
       if (response.result && response.result !== 'error') {
+        this.error = null;
         this.submitted = true;
       } else {
         this.error = response.msg;
